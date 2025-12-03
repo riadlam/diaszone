55823ec73cbcaa250c63e5bee076a53bb355599c
riadlam <riadlaamari@gmail.com>
2025-12-03 17:20:31 +0100

    Fix multi-language system: add Session import, remove duplicate dropdown JS, fix translation files structure

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
