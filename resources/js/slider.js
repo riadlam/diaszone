eb2543610a8c6c4b762afc5a476e8719de8f2c03
riadlam <riadlaamari@gmail.com>
2026-08-20 16:43:37 +0100

    df

--- resources/js/slider.js
+++ resources/js/slider.js
@@ -35,7 +35,7 @@ function initCoverflowHero() {
         },
         autoplay: slides.length > 1
             ? {
-                delay: 3500,
+                delay: 5000,
                 disableOnInteraction: false,
                 pauseOnMouseEnter: true,
             }
--- resources/views/components/hero-slider.blade.php
+++ resources/views/components/hero-slider.blade.php
@@ -5,7 +5,7 @@
 
 @if($heroSlides->isNotEmpty())
 <section id="diaszone-hero-slider" class="dz-coverflow relative w-full overflow-hidden py-6 md:py-10" aria-label="{{ __('home.hero_title') }}">
-    <div class="swiper diaszone-swiper relative z-0 mx-auto rounded-xl max-w-[95%] xs:max-w-[380px] lg:max-w-[906px] h-[162px] lg:h-[385px]"
+    <div class="swiper diaszone-swiper relative z-0 mx-auto rounded-xl max-w-[95%] xs:max-w-[456px] lg:max-w-[906px] h-[194px] lg:h-[385px]"
          style="overflow: visible;">
         <div class="swiper-wrapper">
             @foreach($heroSlides as $index => $slide)
@@ -26,14 +26,14 @@
                         @if($img)
                             <img src="{{ $img }}"
                                  alt="{{ $alt }}"
-                                 class="dz-coverflow__img object-cover rounded-xl w-full h-[162px] lg:h-[385px]"
+                                 class="dz-coverflow__img object-cover rounded-xl w-full h-[194px] lg:h-[385px]"
                                  width="906"
                                  height="385"
                                  loading="{{ $index === 0 ? 'eager' : 'lazy' }}"
                                  fetchpriority="{{ $index === 0 ? 'high' : 'auto' }}"
                                  decoding="async">
                         @else
-                            <div class="rounded-xl w-full h-[162px] lg:h-[385px] bg-gradient-to-br from-purple-700 to-fuchsia-700"></div>
+                            <div class="rounded-xl w-full h-[194px] lg:h-[385px] bg-gradient-to-br from-purple-700 to-fuchsia-700"></div>
                         @endif
                         <div class="dz-coverflow__dim absolute inset-0 rounded-xl bg-black/60 pointer-events-none" aria-hidden="true"></div>
                     @if($hasLink)
